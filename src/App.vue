508db4e894eee193d8e3cf8654afaa75f95bf8b7
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-11-30 15:25:52 +0100

    add: stars

--- src/App.vue
+++ src/App.vue
@@ -69,4 +69,5 @@ export default {
 
 <style lang="scss">
 @use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
 </style>
--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -10,13 +10,26 @@ export default {
     },
     methods: {
         getFlagPath(language) {
-           return new URL(`../assets/img/${language}-flag.png`, import.meta.url).href
+            return new URL(`../assets/img/${language}-flag.png`, import.meta.url).href
         },
     },
     computed: {
         isFlagImageExist() {
-           return this.flag.includes(this.film.original_language);
+            return this.flag.includes(this.film.original_language);
         },
+        backdropUrl() {
+            return this.film.backdrop_path
+                ? `https://image.tmdb.org/t/p/w342${this.film.backdrop_path}`
+                : 'https://via.placeholder.com/342x192?text=No+Image';
+        },
+        filledStars() {
+            const filledStars = Math.ceil(this.film.vote_average / 2);
+            return Array(filledStars).fill('filled-star');
+        },
+        emptyStars() {
+            const emptyStars = 5 - Math.ceil(this.film.vote_average / 2);
+            return Array(emptyStars).fill('empty-star');
+        }
     },
 };
 </script>
@@ -24,16 +37,22 @@ export default {
 <template>
     <div>
         <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="...">
+            <img :src="backdropUrl" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ film.title ? film.title : film.name }}</h5>
                 <h5 class="card-title">{{ film.original_title ? film.original_title : film.original_name }}</h5>
-                <img v-if="isFlagImageExist"  :src="getFlagPath(film.original_language)" alt="">
+                <img v-if="isFlagImageExist" :src="getFlagPath(film.original_language)" alt="">
                 <p v-else class="card-text">{{ film.original_language }}</p>
-                <p class="card-text">{{ film.vote_average }}</p>
+                <span v-for="(star) in filledStars"><i class="fa-solid fa-star"></i></span>
+                <span v-for="(star) in emptyStars"><i class="fa-regular fa-star"></i></span>
             </div>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+    i {
+        color: black;
+    }
+</style>
